fc06e038a6f954653cbb7e9d576e114311d3f735
Rhythm Bhandari <69192521+rhythmbhandari@users.noreply.github.com>
2022-01-02 14:15:03 +0545

    Add project files.

---
+++ Coursework/Form1.Designer.cs
@@ -0,0 +1,117 @@
+﻿
+namespace Coursework
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabHomePage = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.tabPage4 = new System.Windows.Forms.TabPage();
+            this.tabHomePage.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabHomePage
+            // 
+            this.tabHomePage.Controls.Add(this.tabPage1);
+            this.tabHomePage.Controls.Add(this.tabPage2);
+            this.tabHomePage.Controls.Add(this.tabPage3);
+            this.tabHomePage.Controls.Add(this.tabPage4);
+            this.tabHomePage.Location = new System.Drawing.Point(0, 0);
+            this.tabHomePage.Name = "tabHomePage";
+            this.tabHomePage.SelectedIndex = 0;
+            this.tabHomePage.Size = new System.Drawing.Size(1165, 568);
+            this.tabHomePage.TabIndex = 0;
+            this.tabHomePage.TabStop = false;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Location = new System.Drawing.Point(4, 25);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(802, 429);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Entry Form";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            this.tabPage1.Click += new System.EventHandler(this.tabPage1_Click);
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 25);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(1157, 539);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Tickets";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.Location = new System.Drawing.Point(4, 25);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage3.Size = new System.Drawing.Size(799, 429);
+            this.tabPage3.TabIndex = 2;
+            this.tabPage3.Text = "Reports";
+            this.tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // tabPage4
+            // 
+            this.tabPage4.Location = new System.Drawing.Point(4, 25);
+            this.tabPage4.Name = "tabPage4";
+            this.tabPage4.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage4.Size = new System.Drawing.Size(802, 429);
+            this.tabPage4.TabIndex = 3;
+            this.tabPage4.Text = "Charts";
+            this.tabPage4.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1157, 559);
+            this.Controls.Add(this.tabHomePage);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Home";
+            this.tabHomePage.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabHomePage;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TabPage tabPage3;
+        private System.Windows.Forms.TabPage tabPage4;
+    }
+}
+
